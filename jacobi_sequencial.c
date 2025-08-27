1948bfe2e7b12d6c4f4d390fcd7e63e6872debbf
marioluci0 <marioluciocollinettijr@hotmail.com>
2025-08-27 15:02:19 -0300

    funcao sequencial pronta

--- jacobi_sequencial.c
+++ jacobi_sequencial.c
@@ -4,7 +4,9 @@ void *jacobi(double **A, double b[], double p) {
   double x0[N] = {0};
   double x[N] = {0};
   double erro = 0;
-
+  struct timespec inicio, fim;
+  
+  clock_gettime(CLOCK_MONOTONIC, &inicio);
   while(erro > CRITERIO_PARADA) {
     for(int i=0;i<N;i++) {
       double aux=0;
@@ -17,5 +19,11 @@ void *jacobi(double **A, double b[], double p) {
       erro = fabs(x[i] - x0[i]);
       
     }
+    
+    for(int i=0;i<N;i++)
+      x0[i] = x[i];
   }
+  clock_gettime(CLOCK_MONOTONIC, &fim);
+  double tempo = (fim.tv_sec - inicio.tv_sec) + (fim.tv_nsec - inicio.tv_nsec) / 1e9;
+  printf("Tempo de execução: %f segundos\n", tempo);
 }
