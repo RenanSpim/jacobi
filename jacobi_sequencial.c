b7909a888c4439946bd2bda0ce9c4ca9d4bf14eb
marioluci0 <marioluciocollinettijr@hotmail.com>
2025-08-27 14:04:17 -0300

    começo do arquivo sequencial

---
+++ jacobi_sequencial.c
@@ -0,0 +1,11 @@
+#include "jacobi_sequencial.h"
+
+void *jacobi(double **A, double b[], double p) {
+  double x0[2000] = {0};
+  double x[2000] = {0};
+  double erro = 0;
+
+  while(erro<=CRITERIO_PARADA) {
+    
+  }
+}
--- jacobi_sequencial.h
+++ jacobi_sequencial.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+#define CRITERIO_PARADA 0.00001
 
 // TODO
 int jacobi_sequencial() {
