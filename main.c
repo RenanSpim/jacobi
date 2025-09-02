dc9d5a385065dc37ecc83d4c687ebe61355416a1
mt-thome <matheusthomedasilva@gmail.com>
2025-09-02 15:36:15 -0300

    Commit para o merge

--- main.c
+++ main.c
@@ -5,29 +5,33 @@
 int main(){
     double A[N][N], b[N], x[N];
     FILE *fp;
-    fp = fopen("input.txt", "r");
+    fp = fopen("input.dat", "rb");
     if (fp == NULL) {
         perror("Erro ao abrir o arquivo");
         return 1;
     }
-    //fread(A, sizeof(double), N*N, fp);
-    //fread(b, sizeof(double), N, fp);
+    
+    // Verificar se leu corretamente
+    size_t elementos_lidos_A = fread(A, sizeof(double), N*N, fp);
+    if (elementos_lidos_A != N*N) {
+        fprintf(stderr, "Erro: esperava %d elementos da matriz, leu %zu\n", N*N, elementos_lidos_A);
+        fclose(fp);
+        return 1;
+    }
+    
+    size_t elementos_lidos_b = fread(b, sizeof(double), N, fp);
+    if (elementos_lidos_b != N) {
+        fprintf(stderr, "Erro: esperava %d elementos do vetor, leu %zu\n", N, elementos_lidos_b);
+        fclose(fp);
+        return 1;
+    }
+    
+    printf("Matriz %dx%d e vetor de %d elementos carregados com sucesso!\n", N, N, N);
     
     for (int i = 0; i < N; i++) {
         x[i] = 0.0;
     }
+    
     fclose(fp);
     return 0;
-  //size é o tamanho da matriz quadrada
-  //está definido aqui como constante somente para testes
-  //int size = 3;
-  //double **A, b[size];
-
-  //A =  (double **)malloc(size * sizeof(double*));
-
-  //for(int i=0;i<size;i++) {
-  //      A[i] = (double *)malloc(size * sizeof(double));
-  //  }
- 
-  //return 0;
 }
