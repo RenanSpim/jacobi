0491c72a3ad06e5eeaef5975c5b11b336bf586a5
Vítor Hugo Lellis Lopes <vhllopes.dev@gmail.com>
2025-09-16 18:11:59 -0300

    Atualiza os algoritmos jacobi paralelo e sequencial

--- jacobi_paralelo.c
+++ jacobi_paralelo.c
@@ -4,18 +4,13 @@
 #include <time.h>
 #include <omp.h>
 
+
 #define N 2000
 #define CRITERIO_PARADA 1e-5
 #define MAX_ITER 10000
 
-typedef enum {
-    STATIC,
-    DYNAMIC,
-    GUIDED
-} ScheduleType;
-
 void lendo(double **A, double *B) {
-  FILE *file = fopen("../assets/gemini10.dat", "r");
+  FILE *file = fopen("sistlinear2k.dat", "r");
 
   for(int i=0;i<N;i++) {
     for(int j=0;j<N;j++) {
@@ -27,59 +22,49 @@ void lendo(double **A, double *B) {
     fscanf(file, "%lf", &B[i]);
     i++;
   }
+
+  fclose(file);
 }
 
-int jacobi_paralelo(double A[N][N], double *b, double *x, ScheduleType type, int chunk_size, int num_threads) {
-    double *x_new = malloc(N * sizeof(double));
-    int iterations = 0;
-    double error = CRITERIO_PARADA + 1.0;
+void jacobi(double **A, double *B, double p) {
+    double x0[N] = {0};
+    double x[N] = {0};
+    double erro = 0.1, erro_atual = -1, erro_max=0;
     struct timespec inicio, fim;
-
-    omp_sched_t kind;
-    switch (type)
-    {
-        case STATIC: kind = omp_sched_static; break;
-        case GUIDED: kind = omp_sched_guided; break;
-        case DYNAMIC: kind = omp_sched_dynamic; break;
-        default: 
-            printf("Digite um tipo correto\n");
-            free(x_new); 
-            return 0;
-            break;
-    }
-    omp_set_num_threads(num_threads);
-    omp_set_schedule(kind, chunk_size);
+    int iteracoes = 0; // ADICIONADO: Declara e inicializa o contador
 
     clock_gettime(CLOCK_MONOTONIC, &inicio);
-    while (iterations++ < MAX_ITER && error > CRITERIO_PARADA) {
-        error = 0.0;
-        #pragma omp parallel for schedule(runtime) reduction(max:error)
-        for (int i = 0; i < N; i++) {
-            double sigma = 0.0;
+    while(erro > p) {
+        for(int i=0;i<N;i++) {
+            double aux=0;
 
-            for (int j = 0; j < N; j++) {
-                if (i == j) continue;
-                sigma += A[i][j] * x[j];
+            for(int j=0;j<N;j++) {
+                if(i==j) continue;
+                aux += A[i][j] * x0[j];
             }
-            x_new[i] = (b[i] - sigma) / A[i][i];
-            double local_error = fabs(x_new[i] - x[i]);
-            error = local_error > error ? local_error : error;
+            x[i] = (B[i] - aux)/A[i][i];
+            erro_atual = fabs(x[i] - x0[i]);
+            
+            if(erro_atual > erro_max)
+                erro = erro_atual;
         }
-        
-        for (int i = 0; i < N; i++) {
-            x[i] = x_new[i];
+    
+        for(int i=0;i<N;i++) {
+            x0[i] = x[i];
         }
+        iteracoes++; // ADICIONADO: Incrementa o contador a cada iteração
     }
     clock_gettime(CLOCK_MONOTONIC, &fim);
+
+    printf("\nConvergência alcançada em %d iterações.\n", iteracoes); // ADICIONADO: Imprime o total de iterações
+    
     double tempo = (fim.tv_sec - inicio.tv_sec) + (fim.tv_nsec - inicio.tv_nsec) / 1e9;
     printf("Tempo de execução: %f segundos\n", tempo);
 
-    FILE *file = fopen("resultado_paralelo.txt", "w");
+    FILE *file = fopen("resultado_sequencial.txt", "w");
 
     if (file == NULL) {
         printf("Erro ao abrir o arquivo para escrita!\n");
-        free(x_new); 
-        return 0; 
     }
 
     fprintf(file, "Resultado final do vetor x:\n");
@@ -88,19 +73,24 @@ int jacobi_paralelo(double A[N][N], double *b, double *x, ScheduleType type, int
     }
 
     fclose(file);
-    free(x_new);
-    return 1;
 }
 
-int main() {
+int main(){
   double **A = (double **)malloc(N * sizeof(double *));
   double *B = (double *)malloc(N * sizeof(double));
-  double *x = (double *)malloc(N * sizeof(double));
 
   for(int i=0;i<N;i++)
     A[i] = (double *)malloc(N*sizeof(double));
-
+  
   lendo(A, B);
-  jacobi_paralelo(A, B, x, STATIC, 1, 4);
+  jacobi(A, B, CRITERIO_PARADA);
+
+  // Libera a memória alocada
+  for(int i = 0; i < N; i++) {
+    free(A[i]); // Libera cada linha da matriz
+  }
+  free(A); // Libera o ponteiro para as linhas
+  free(B); // Libera o vetor B
+
   return 0;
 }
